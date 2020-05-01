6886696bca8538793b69f0a961f26e3b7001bec9
LucaArgentieri <50273216+LucaArgentieri@users.noreply.github.com>
2020-05-01 17:33:25 +0200

    Update Dati

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 
 document.addEventListener('DOMContentLoaded', (evt) => {
 
-  let confirmeddata = document.getElementById('confirmeddata')
-  let recovereddata = document.getElementById('recovereddata')
-  let deathsdata = document.getElementById('deathsdata')
-  let lastUpdatedata = document.getElementById('lastUpdatedata')
-
-
-
   var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
-  fetch("https://covid19.mathdro.id/api/countries/IT"
+  fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json   "
     , requestOptions)
     .then(response => response.json())
     .then(data => {
-      let confirmed = data.confirmed.value
-      let recovered = data.recovered.value
-      let deaths = data.deaths.value
-      let lastUpdate = data.lastUpdate
-      
+      let tempo = data[0].data
+      console.log(tempo)
+      let deceduti = data[0].deceduti
+      console.log(deceduti)
+      let dimessi_guariti = data[0].dimessi_guariti
+      console.log(dimessi_guariti)
+      let isolamento_domiciliare = data[0].isolamento_domiciliare
+      console.log(isolamento_domiciliare)
+      let nuovi_positivi = data[0].nuovi_positivi
+      console.log(nuovi_positivi)
+      let ricoverati_con_sintomi = data[0].ricoverati_con_sintomi
+      console.log(ricoverati_con_sintomi)
+      let tamponi = data[0].tamponi
+      console.log(tamponi)
+      let terapia_intensiva = data[0].terapia_intensiva
+      console.log(terapia_intensiva)
+      let totale_casi = data[0].totale_casi
+      console.log(totale_casi)
+      let totale_ospedalizzati = data[0].totale_ospedalizzati
+      console.log(totale_ospedalizzati)
+      let totale_positivi = data[0].totale_positivi
+
 
 //chart.js
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Positivi al Covid', 'Guariti', 'Deceduti'],
+        labels: [tempo],
         datasets: [{
             label: 'Popolazione Italiana',
             data: [
-              confirmed,
-              recovered,
-              deaths
+              deceduti,
             ],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
