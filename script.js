bbf46c1101023d1115fc26493e0181bf5527d6cc
LucaArgentieri <50273216+LucaArgentieri@users.noreply.github.com>
2020-05-02 11:20:33 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', (evt) => {
 
+
   var requestOptions = {
     method: 'GET',
     redirect: 'follow'
@@ -8,75 +9,94 @@ document.addEventListener('DOMContentLoaded', (evt) => {
   
   fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json   "
     , requestOptions)
-    .then(response => response.json())
+    .then(response => {
+      return response.json()
+    })
     .then(data => {
-    console.log(data)
-      function usageGraph() {
-        var xvals = [],
-          yvals = [],
-          yvalsEst = [];
-        for (var i = 0; i < lenght(json.data); i++) {
-          xvals[i] = formatDateMonth(new Date(json.data[i].day));
-          yvals[i] = json.data[i].km * 1;
-          yvalsEst[i] = json.analyzedData[i].estKm;
-        }
-        console.log(xvals[i])
-    }
+    console.log(data);
+
+
+    
       let tempo = data[0].data
-      console.log(tempo)
       let deceduti = data[0].deceduti
-      console.log(deceduti)
       let dimessi_guariti = data[0].dimessi_guariti
-      console.log(dimessi_guariti)
       let isolamento_domiciliare = data[0].isolamento_domiciliare
-      console.log(isolamento_domiciliare)
       let nuovi_positivi = data[0].nuovi_positivi
-      console.log(nuovi_positivi)
       let ricoverati_con_sintomi = data[0].ricoverati_con_sintomi
-      console.log(ricoverati_con_sintomi)
       let tamponi = data[0].tamponi
-      console.log(tamponi)
       let terapia_intensiva = data[0].terapia_intensiva
-      console.log(terapia_intensiva)
       let totale_casi = data[0].totale_casi
-      console.log(totale_casi)
       let totale_ospedalizzati = data[0].totale_ospedalizzati
-      console.log(totale_ospedalizzati)
       let totale_positivi = data[0].totale_positivi
-      console.log(totale_positivi)
+
+
 
 
 //chart.js
-const ctx = document.getElementById('myChart').getContext('2d');
-const xlabels = [];
-const myChart = new Chart(ctx, {
+var myChart1 = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(myChart1, {
     type: 'line',
     data: {
-        labels: [tempo],
+        labels: [tempo], //tempo
         datasets: [{
-            label: '# of Votes',
-            data: [deceduti],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }],
-
+            label:"Deceduti",
+            data: [deceduti], // categoria
+            backgroundColor: ['rgba(255, 99, 132, 0.2)',],
+        },
+        {
+          label:"Dimessi guariti",
+          data: [dimessi_guariti], // categoria
+          backgroundColor: ['rgba(255, 99, 71, 0.5)'],
+        },
+        {
+          label:"Isolamento domiciliare",
+          data: [isolamento_domiciliare], // categoria
+          backgroundColor: ['rgba(255, 99, 255, 0.5)'],
+        },
+        {
+          label:"Nuovi positivi",
+          data: [nuovi_positivi], // categoria
+          backgroundColor: ['rgba(255, 172, 0, 0.5)'],
+        },
+        {
+          label:"Ricoverati con sintomi",
+          data: [ricoverati_con_sintomi], // categoria
+          backgroundColor: ['rgba(0, 172, 0, 1)'],
+        },
+        {
+          label:"Tamponi effettuati",
+          data: [tamponi], // categoria
+          backgroundColor: ['rgba(162, 0, 0, 1)'],
+        },
+        {
+          label:"Terapia intensiva",
+          data: [terapia_intensiva], // categoria
+          backgroundColor: ['rgba(162, 0, 252, 1)'],
+        },
+        {
+          label:"Casi totali",
+          data: [totale_casi], // categoria
+          backgroundColor: ['rgba(162, 144, 0, 1)'],
+        },
+        {
+          label:"Totale ospedalizzati",
+          data: [totale_ospedalizzati], // categoria
+          backgroundColor: ['rgba(0, 21, 164, 1)'],
+        },
+        {
+          label:"Totale positivi",
+          data: [totale_positivi], // categoria
+          backgroundColor: ['rgba(0, 255, 164, 1)'],
+        },
+      ]
     },
     options: {
+      responsive:true,
+      maintainAspectRatio: false,
+      title: {
+        text: "Prova titolo",
+        display: true,
+      },
         scales: {
             yAxes: [{
                 ticks: {
@@ -89,12 +109,8 @@ const myChart = new Chart(ctx, {
 
 
 
-
-
-    })
-    .catch(error => console.log('error', error));
-
-
+})
+.catch(error => console.log('error', error));
 
 
 
